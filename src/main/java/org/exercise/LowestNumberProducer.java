7f819c5058da87564c2a5ad5326ea61459bebb50
maciej <bandurskim@gmail.com>
2020-05-01 14:01:40 +0200

    Added documentation for LowestNumberProducer.findLowestPositiveNumber method

--- src/main/java/org/exercise/LowestNumberProducer.java
+++ src/main/java/org/exercise/LowestNumberProducer.java
@@ -4,6 +4,13 @@ import java.util.Arrays;
 
 public class LowestNumberProducer {
 
+  /**
+   * Given an array of integers, find the first missing positive integer in linear time and constant space.
+   * In other words, find the lowest positive integer that does not exist in the array.
+   * The array can contain duplicates and negative numbers as well.
+   * <p>
+   * For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should give 3.
+   */
   public static int findLowestPositiveNumber(int[] array) {
     int[] sortedPositiveNumbers = Arrays.stream(array)
       .distinct()
